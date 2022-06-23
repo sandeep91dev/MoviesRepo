7c6404b6103c3dff498bf105ddcbc0b0fb978a8f
sandeep91dev <sndpshtt881@gmail.com>
2022-06-23 10:30:49 +0530

    Create DB connection using typeorm

--- src/films/dto/create-film.dto.ts
+++ src/films/dto/create-film.dto.ts
@@ -1,1 +1,11 @@
-export class CreateFilmDto {}
+import { IsNotEmpty, Length } from 'class-validator';
+
+export class CreateFilmDto {
+      @IsNotEmpty()
+      @Length(3, 255)
+      question: string;
+    
+
+      @IsNotEmpty()
+      quizId: number;
+}
