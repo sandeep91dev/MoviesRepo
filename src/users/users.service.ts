2201ce558e26a1831d0284d2abc0a6bdf6775f3f
sandeep91dev <sndpshtt881@gmail.com>
2022-06-23 10:34:34 +0530

    DB connection using typeorm

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,13 +2,11 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
-import { UserRepository } from './users.repository';
 import { User } from './entities/user.entity';
 @Injectable()
 export class UsersService {
 
   constructor(
-    @InjectRepository(UserRepository) private userRepository:UserRepository
   ){}
 
   create(createUserDto: CreateUserDto) {
